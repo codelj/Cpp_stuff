ed4e3c30bca86f31f4a9107ad1e26bd528c5413d
codelj <rey.ljian@yahoo.com>
2013-05-26 02:16:48 -0300

    Create GifMovieGear_KeyG.c

---
+++ GifMovieGear_KeyG.c
@@ -0,0 +1,24 @@
+/*
+*Author: L.J
+*Date: April.1st , 2012
+*eMail: lei.jianray@gmail.com
+*/
+
+int main()
+{
+  int i=0,lisence=0;
+  char name[256];
+  printf("        ************\n Type any name, please :) <3     :  ");
+  scanf("%s",name);
+  system("cls");
+  while(name[i]!=0)
+  {
+    lisence+=name[i]*(++i);
+  }
+  printf("        ************\n");
+  printf("User Name: %s\n",name);
+  printf("lisence  : mg37s610%d\n\n",lisence+3039);
+  printf("  by: L.J.Masker           Var:1.0 Mx\n");
+  getch();
+  return 0;
+}
